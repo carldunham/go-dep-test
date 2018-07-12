7b2608f48052307ef9e25257a5492661866ba852
Carl Dunham <cdunham@gmail.com>
2018-07-12 16:03:57 -0400

    initial commit

---
+++ src/apps/appA/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"libs/coolstuff"
+)
+
+func main() {
+	fmt.Println(coolstuff.A())
+}
---
+++ src/libs/coolstuff/coolstuff.go
@@ -0,0 +1,7 @@
+package coolstuff
+
+// A is pretty cool
+//
+func A() string {
+	return "💯"
+}
